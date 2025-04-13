38ec20c19f73f937197d30b74c549f07b06f2190
unknown <abiodunigbehinadun17@gmail.com>
2025-04-13 02:06:10 +0100

    first commit

---
+++ modules/custom/starrlisting/src/Plugin/Block/AboutBlock.php
@@ -0,0 +1,33 @@
+<?php
+namespace Drupal\starrlisting\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides an 'About' Block.
+ *
+ * @Block(
+ *   id = "about_block",
+ *   admin_label = @Translation("About Block"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class AboutBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'about_block',
+    //   '#attributes' => [
+    //     'class' => ['custom-about-block'],
+    //   ],
+    //   '#attached' => [
+    //     'library' => [
+    //       'my_custom_blocks/about_block_styles',
+    //     ],
+    //   ],
+    ];
+  }
+
+}
